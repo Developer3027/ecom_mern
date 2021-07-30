d66a36f9f2f2becffe9332eed10c8ff02dbde20b
mason roberts <mrobe18@gmail.com>
2021-07-30 13:03:13 -0400

    created orders endpoint to post to db

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDb from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 connectDb();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
